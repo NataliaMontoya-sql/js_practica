0bff2c94e9f88cf66066b5e00c9d10a5e62a3fa7
Natalia Montoya <nataliajaramillo1139598@correo.itm.edu.co>
2025-02-18 13:06:50 -0500

    Update basis.py

--- basis.py
+++ basis.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-
+import matplotlib.pyplot as mpl
 # Título y descripción
 st.title("Dashboard Educativo Interactivo")
 st.subheader("Una herramienta de aprendizaje divertida")
